efaa9bd90bddd028af14d74cc87f6200c1132c49
Octo100 <kevawes494@keagenan.com>
2021-11-27 09:46:57 +0530

    Added navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export let Navbar = () => {
 
     return(
         <div className="min-h-full" >
-            <nav className="flex items-center justify-between flex-wrap bg-gradient-to-tr from-green-900 via-purple-800 to-green-700  p-2">
+            <nav className="flex items-center justify-center flex-wrap bg-gradient-to-tr from-green-900 via-purple-800 to-green-700  p-2">
                 <div className="flex items-center flex-shrink-0 text-white mr-6">
                     <Logo className="h-12 w-12 mr-5 animate-pulse md:hidden"></Logo>
                     <span className="font-bold text-2xl tracking-tight ">Cipher Control Panel</span>
@@ -28,15 +28,20 @@ export let Navbar = () => {
 
                 </div>
                 <div className={`w-full ${ nbo ? 'hidden' : 'block'} flex-grow lg:flex lg:items-center lg:w-auto`} id="navbar">
-                        <button
-                            className="inline-flex items-center h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
-                            <svg className="w-4 h-4 mr-3 fill-current" viewBox="0 0 20 20">
-                                <path
-                                    d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                                    clip-rule="evenodd" fill-rule="evenodd"></path>
-                            </svg>
-                            <span>With icon</span>
-                        </button>
+                    <div class="text-sm lg:flex-grow  text-center lg:text-right">
+                        <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4 text-lg">
+                            Home
+                        </a>
+                        <a href="https://github.com/raghavyuva/optimize-prime" class="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4 text-lg">
+                            About
+                        </a>
+                        <a href="" class="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4 text-lg">
+                            Support
+                        </a>
+                        <a href="#donate" class="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white text-lg">
+                            Donate
+                        </a>
+                    </div>
                 </div>
 
             </nav>
