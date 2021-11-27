7a24ef87be9b44dbf9929a34627deea77d5c14f6
Octo100 <kevawes494@keagenan.com>
2021-11-27 11:42:44 +0530

    Added charts

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SideBarContents } from "./components/Constants/SideBarContents";
 import ListOfUsers from "./components/ListOfUsers";
 import DefCodePath from "./components/DefCodePath";
-<<<<<<< HEAD
 import {LoginScreen} from "./components/LoginScreen";
-=======
-import Home from './components/Landing/Home';
 import Chart from './components/Chart';
 import Chart02 from './components/Chart02';
 import StatsSection from './components/StatsSection';
->>>>>>> 869532a (landing page and with dash components)
-
+import {WidgetGridBox} from "./components/Widgets";
 
 function MainScreen() {
   const [Toggle, setToggle] = useState(false);
@@ -44,7 +40,7 @@ function MainScreen() {
     },
   ]
   return (<div >
-    {/* <DefCodePath
+  <DefCodePath
       Components={Cards}
       Toggle={Toggle}
       setToggle={setToggle}
@@ -52,18 +48,20 @@ function MainScreen() {
       setbannerOpen={setbannerOpen}
       sidebarContents={SideBarContents}
       grid_system={true}
-    /> */}
-    <Home />
+    />
   </div>)
 }
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<MainScreen />} />
-      </Routes>
-    </BrowserRouter>
+
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<MainScreen/>} />
+          <Route exact path="/login" element={<LoginScreen/>}/>
+          <Route exact path="/widgets" element={<WidgetGridBox/>}/>
+        </Routes>
+      </BrowserRouter>
   )
 };
 
